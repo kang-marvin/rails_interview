f9a40af874cd50c2e9e51d6ea3635ec3ed11a099
Marvin Kang'ethe <marvinjameskang@gmail.com>
2022-04-07 16:30:50 +0300

    Spec for People controller

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -4,9 +4,23 @@ class PeopleController
   end
 
   def normalize
+    wanted_values = [ 'first_name', 'city', 'birthdate' ]
+    merged_hash_content = [
+      data({ file_content: @params[:dollar_format] }),
+      data({ file_content: @params[:percent_format], separator: "%" }),
+    ]
+
+    merged_hash_content.flatten.sort_by!{ |obj| obj[@params[:order].to_s] }.map do |obj|
+      obj.values_at(*wanted_values).join(", ")
+    end
   end
 
   private
+
+  def data(options = {})
+    return [] if options.values.any?(&:nil?)
+    ::HashifyParsedFileContent.new(options).call
+  end
 
   attr_reader :params
 end
