f9475236c7883b5af5446bc965d840b8e4fca7c2
Marvin Kang'ethe <marvinjameskang@gmail.com>
2022-04-07 15:17:59 +0300

    Return nil if birth date is not provided or is empty

--- helper/birth_date_format.rb
+++ helper/birth_date_format.rb
@@ -7,11 +7,12 @@ class BirthDateFormat
   # The minus (-) is used to remove appending zero
   FORMAT_TO_USE = "%-m/%-d/%Y"
 
-  def initialize(birth_date)
+  def initialize(birth_date = '')
     @birth_date = birth_date
   end
 
   def call
+    return nil unless present?(@birth_date)
     format_birth_date
   end
 
@@ -19,6 +20,10 @@ class BirthDateFormat
 
   def format_birth_date
     Date.parse(@birth_date).strftime(FORMAT_TO_USE)
+  end
+
+  def present?(str)
+    !str.to_s.empty?
   end
 
 end
