6711542d6c0bfd14fb52dd36a3453f20d513b8d0
Marvin Kang'ethe <marvinjameskang@gmail.com>
2022-04-07 17:07:33 +0300

    Include all the files during boot

--- boot.rb
+++ boot.rb
@@ -1,1 +1,11 @@
+# Controllers
 require './app/people_controller.rb'
+
+# Service Objects
+require './service/hashify_parsed_file_content.rb'
+
+# Data Files
+require './data/city_data.rb'
+
+# Helper Files
+require './helper/birth_date_format.rb'
